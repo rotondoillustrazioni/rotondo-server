59e6d1ebd1b64e7ed58ba730b80a50ba2b5abf46
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-06-19 14:00:35 +0200

    fix

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.get("/projects", async (req, res) => {
 //   });
 // });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening");
 });
