d3b94ac588590edfc599d8093e1c41827d35571f
Linda Vitali <linda.vitali2@studio.unibo.it>
2023-01-09 13:10:26 +0100

    refactor: string

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ app.get("/project/:id", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening...");
 });
